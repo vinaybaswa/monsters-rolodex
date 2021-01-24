a389823586f86e2ce8df90df469d8f267b57ee90
vinay baswa <vinaybaswa@gmail.com>
2021-01-24 00:32:44 -0500

    Card styling and fetch images

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import './card-list.css'
+import './card-list.styles.css';
+import { Card } from './card.component';
 
 export const Cardlist = props => {
   return (
     <div className='card-list'>
       {props.monsters.map(monster => {
-        return (
-          <h1 key={monster.id}>{monster.name}</h1>
-        )
+        return <Card key={monster.id} monster={monster} />
       })}
     </div>)
 }
