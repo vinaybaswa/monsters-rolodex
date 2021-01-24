e2abeb43f539f6f20cc39f0f9b08488c36d9ec44
vinay baswa <vinaybaswa@gmail.com>
2021-01-23 23:50:37 -0500

    transfer card-list mapping function to Cardlist

--- src/App.js
+++ src/App.js
@@ -21,13 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Cardlist>
-          {this.state.monsters.map(monster => {
-            return (
-              <h1 key={monster.id}> {monster.name} </h1>
-            )
-          })}
-        </Cardlist>
+        <Cardlist monsters={this.state.monsters} />
       </div>
     )
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,5 +2,12 @@ import React from 'react';
 import './card-list.css'
 
 export const Cardlist = props => {
-  return <div className='card-list'>{props.children}</div>
+  return (
+    <div className='card-list'>
+      {props.monsters.map(monster => {
+        return (
+          <h1 key={monster.id}>{monster.name}</h1>
+        )
+      })}
+    </div>)
 }
