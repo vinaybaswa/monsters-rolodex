5cd559e92bc3e263b156c1d3af885915616a5f8f
vinay baswa <vinaybaswa@gmail.com>
2021-01-26 00:31:06 -0500

    separate the search box component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import './App.css';
 
-import { CardList } from './components/card/card-list.component.jsx'
+import { CardList } from './components/card/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
@@ -27,10 +28,9 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input
-          type='search'
+        <SearchBox
           placeholder='search monster'
-          onChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={e => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
       </div>
